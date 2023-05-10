71da73b2e1d8b33f2319d7a55cdfffa036996019
thuan.duong <thuan.duong@ststechnology.com>
2023-05-10 10:11:53 +0700

    redirect default routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
   { path: 'products', loadChildren: () => import('./product/product.module').then(m => m.ProductModule) },
 ];
 
